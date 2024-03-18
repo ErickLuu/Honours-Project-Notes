87f1edc684fb499837537dd101a80a13eb53088b
Eric Luu <eluu0002@student.monash.edu>
2024-03-18 18:59:23 +1100

    Fixed problem 3

--- Notes/Combinatorics/Assignment 1/Problem 4.py
+++ Notes/Combinatorics/Assignment 1/Problem 4.py
@@ -13,10 +13,11 @@ def queue(n: int) -> int:
         '''
         Count is from 0 to n inclusive. Calculates values from paper.
         '''
-        count += (-1)**k * fc(n) * fc(2* n - k)* 2**k /fc(k)/fc(n-k)
+        count += (-1)**k * binom(n, k)* fc(2* n - k)* 2**k
     return count/2**n
 
 def diffq5(n: int, k: int) -> int:
     return binom(n,2)**2 - k * n * (n-1)**2 + 2 * n * binom(k, 2) * (n-1) + n * k * (n * k - 2 * k + 1)/2 - 2 * n * binom(k, 2)*(k-1)
 
-print(diffq5(3, 2))
+#print(diffq5(3, 2))
+print(queue(2))
